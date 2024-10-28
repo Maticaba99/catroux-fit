98ef18fffbb8542568adb9960ef91870bf2400ca
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-27 23:30:36 -0300

    a

--- src/app/components/objesiones.tsx
+++ src/app/components/objesiones.tsx
@@ -2,7 +2,7 @@ import { FaQuestionCircle } from "react-icons/fa";
 
 const Faq = () => {
   return (
-    <div className="bg-gray-800">
+    <div className="bg-gray-800 -translate-y-6 translate-y-6">
       {" "}
       {/* Cambiado de <objesiones> a <div> */}
       <div className="bg-gray-900 text-white p-6 rounded-lg max-w-6xl mx-auto">
