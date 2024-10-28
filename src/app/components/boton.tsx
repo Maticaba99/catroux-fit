4ac17d9496f1dcf497f8e18120704c5313b0d8c8
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-28 00:16:20 -0300

    ah

--- src/app/components/boton.tsx
+++ src/app/components/boton.tsx
@@ -3,8 +3,11 @@ function Boton() {
     <div>
       <div className="bg-gray-800">
         <div class="flex justify-center items-center min-h-screen">
-          <button class="-translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300">
-            Hacelo centrado en el medio
+          <button className="-translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300">
+            <span className="text-lg">Reserva una llamada conmigo</span>
+            <span className="text-sm mt-1 opacity-80">
+              y empieza tu transformación
+            </span>
           </button>
         </div>
       </div>
