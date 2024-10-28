0390dc1b3eb2c70ca911785b78d1796da77b2287
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-27 23:53:25 -0300

    botton

--- src/app/components/objesiones.tsx
+++ src/app/components/objesiones.tsx
@@ -162,6 +162,12 @@ const Faq = () => {
                 maximizar tus resultados. Estoy comprometido a guiarte hasta
                 alcanzar tus objetivos.{" "}
               </p>
+              <button className="-translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
+                <span className="text-lg">Reserva una llamada conmigo</span>
+                <span className="text-sm mt-1 opacity-80">
+                  y empieza tu transformación
+                </span>
+              </button>
             </div>
           </div>
         </div>
