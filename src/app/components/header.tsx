99eebb1c3a91ec5c7337edcd10bec2ae6faa26d6
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 03:17:51 -0300

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,11 +3,8 @@ import React from "react";
 
 function Header() {
   return (
-    <header
-      className="bg-gray-900 text-white pt-6
-"
-    >
-      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-center items-center">
+    <header className="bg-gray-900 text-white pt-6">
+      <div className="max-w-7xl mx-auto px-4 flex justify-center items-center">
         <p className="flex items-center">CATROUX</p>
       </div>
     </header>
