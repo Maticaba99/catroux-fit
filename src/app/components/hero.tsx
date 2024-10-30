e5720e980995ec6302e40f242945703b0264fc41
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 02:34:17 -0300

    gsddgs

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className="bg-gray-800">
       <section className="flex flex-col items-center text-center space-y-4 max-w-2xl mx-auto">
         <div style={{ fontFamily: "Poppins, sans-serif" }}>
-          <h1 className="text-5xl  text-white m-4 uppercase">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl  text-white m-4 uppercase">
             Te enseño cómo cambiar tu físico y a seguir mejorándolo por ti mismo
             para no depender de un entrenador
           </h1>
