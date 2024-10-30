4bb1783c925c7371c4f61254a6cd6230b012743f
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 05:02:08 -0300

    fas

--- src/app/components/objesiones.tsx
+++ src/app/components/objesiones.tsx
@@ -4,7 +4,7 @@ import "@fontsource/poppins";
 const Faq = () => {
   return (
     <div
-      className="bg-gray-800"
+      className="bg-gray-800 space-y-4 max-w-2xl"
       style={{
         padding: "20px",
         color: "#fff",
@@ -62,7 +62,10 @@ const Faq = () => {
                   resultados?
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl ">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl "
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Los resultados visibles pueden comenzar en unas pocas semanas,
                 pero mi objetivo es enseñarte a mantenerlos a largo plazo.
                 Trabajaremos juntos hasta que te sientas seguro para manejarlo
@@ -74,11 +77,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Qué sucede después de finalizar la asesoría?
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Habrás aprendido a gestionar tu nutrición y entrenamiento de
                 manera autónoma, evitando la necesidad de una asesoría continua.
               </p>
@@ -88,11 +97,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Voy a tener que hacer dieta estricta?
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 No, crearemos un plan con alimentos que disfrutes y que ya sean
                 parte de tu vida diaria, ajustados a tus objetivos.{" "}
               </p>
@@ -101,11 +116,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Cómo puedo saber si el plan funcionará para mí?{" "}
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Diseñaremos un plan basado en tus preferencias y necesidades
                 específicas, asegurándonos de que sea sostenible y efectivo para
                 ti.{" "}
@@ -115,11 +136,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Qué pasa si nunca he seguido un plan de entrenamiento?{" "}
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Te guiaré paso a paso, enseñándote las técnicas y el enfoque
                 correcto para que desarrolles confianza y conocimiento.{" "}
               </p>
@@ -128,12 +155,18 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Qué diferencia hay entre tus asesorías y las de otros
                   entrenadores?{" "}
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Mi enfoque es enseñarte a gestionar tu físico por ti mismo, para
                 que no dependas de asesorías a largo plazo.{" "}
               </p>
@@ -142,11 +175,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Puedo adaptar mi plan si mi vida cambia?{" "}
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Por supuesto. Tu plan será flexible y se ajustará a tu estilo de
                 vida y cambios, garantizando su efectividad en cualquier
                 situación.{" "}
@@ -156,11 +195,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Cuántas veces a la semana debo entrenar?{" "}
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Diseñaremos un plan que se adapte a tus horarios y estilo de
                 vida, garantizando que puedas mantener la constancia.{" "}
               </p>
@@ -169,11 +214,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Y si no tengo experiencia en el gimnasio?{" "}
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 No te preocupes. Mis asesorías están diseñadas para todos los
                 niveles, desde principiantes hasta avanzados.{" "}
               </p>
@@ -182,11 +233,17 @@ const Faq = () => {
             <div className="bg-gray-800 p-4 rounded-lg">
               <div className="flex items-center">
                 <FaQuestionCircle className="text-gray-400 mr-2" />
-                <h3 className="font-semibold">
+                <h3
+                  className="font-semibold"
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                >
                   ¿Qué pasa si no veo resultados en un tiempo razonable?{" "}
                 </h3>
               </div>
-              <p className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl">
+              <p
+                className="mt-2 text-gray-300 mt-2 space-y-4 max-w-3xl"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
                 Revisaremos juntos el plan y haremos los ajustes necesarios para
                 maximizar tus resultados. Estoy comprometido a guiarte hasta
                 alcanzar tus objetivos.{" "}
