0dc8e34567a64cd3c7d9a6d71ac74cf12a5bf7cd
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 03:15:13 -0300

    asffas

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,10 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-gray-900 text-white py-8">
+    <header
+      className="bg-gray-900 text-white pt-6
+"
+    >
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-center items-center">
         <p className="flex items-center">CATROUX</p>
       </div>
