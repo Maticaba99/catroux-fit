bc90abf6463b5feeb7a02a3938a32bdd869f8478
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 03:59:18 -0300

    Update hero.tsx

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -29,12 +29,17 @@ function Hero() {
             allowFullScreen
           ></iframe>
         </div>
-        <button className="-translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-5 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
-          <span className="text-lg">Reserva una llamada conmigo</span>
-          <span className="text-sm mt-1 opacity-80">
-            y empieza tu transformación
-          </span>
-        </button>
+        <section
+          className="py-8
+"
+        >
+          <button className="-translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-5 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
+            <span className="text-lg">Reserva una llamada conmigo</span>
+            <span className="text-sm mt-1 opacity-80">
+              y empieza tu transformación
+            </span>
+          </button>
+        </section>
       </section>
     </div>
   );
