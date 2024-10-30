ef1c6a13092d1568d7c95ed1959edf1257f6dfd7
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 04:29:17 -0300

    Update abjao.tsx

--- src/app/components/abjao.tsx
+++ src/app/components/abjao.tsx
@@ -24,76 +24,81 @@ function Features() {
           <div className="mt-12 space-y-8">
             {/* Feature 1 */}
             <div className="flex">
-              <div className="flex-shrink-0"></div>
-              <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
-              <div className="ml-4">
-                <h3 className="text-2xl font-semibold text-white">
-                  Independencia para el Futuro{" "}
-                </h3>
-                <p className="text-gray-600 mt-2 text-white">
-                  Aprenderás a gestionar tu físico por ti mismo, evitando la
-                  dependencia continua de asesorías.{" "}
-                </p>
+              <div className="flex-shrink-0">
+                <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
+                <div className="ml-4">
+                  <h3 className="text-2xl font-semibold text-white">
+                    Independencia para el Futuro{" "}
+                  </h3>
+                  <p className="text-gray-600 mt-2 text-white">
+                    Aprenderás a gestionar tu físico por ti mismo, evitando la
+                    dependencia continua de asesorías.{" "}
+                  </p>
+                </div>
               </div>
             </div>
 
             <div className="flex">
-              <div className="flex-shrink-0"></div>
-              <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
-              <div className="ml-4">
-                <h3 className="text-2xl font-semibold text-white">
-                  Desarrollo de Conocimientos Reales{" "}
-                </h3>
-                <p className="text-white-600 mt-2 text-white">
-                  Obtendrás herramientas prácticas que te permitirán tomar
-                  decisiones con seguridad, incluso después de nuestro trabajo
-                  juntos.{" "}
-                </p>
+              <div className="flex-shrink-0">
+                <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
+                <div className="ml-4">
+                  <h3 className="text-2xl font-semibold text-white">
+                    Desarrollo de Conocimientos Reales{" "}
+                  </h3>
+                  <p className="text-white-600 mt-2 text-white">
+                    Obtendrás herramientas prácticas que te permitirán tomar
+                    decisiones con seguridad, incluso después de nuestro trabajo
+                    juntos.{" "}
+                  </p>
+                </div>
               </div>
             </div>
 
             <div className="flex">
-              <div className="flex-shrink-0"></div>
-              <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
-              <div className="ml-4">
-                <h3 className="text-2xl font-semibold text-white">
-                  Estrategias Basadas en Resultados{" "}
-                </h3>
-                <p className="text-gray-600 mt-2 text-white">
-                  Diseñadas para cambios visibles, sostenibles y en un tiempo
-                  optimizado.{" "}
-                </p>
+              <div className="flex-shrink-0">
+                <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
+                <div className="ml-4">
+                  <h3 className="text-2xl font-semibold text-white">
+                    Estrategias Basadas en Resultados{" "}
+                  </h3>
+                  <p className="text-gray-600 mt-2 text-white">
+                    Diseñadas para cambios visibles, sostenibles y en un tiempo
+                    optimizado.{" "}
+                  </p>
+                </div>
               </div>
             </div>
 
             <div className="flex">
-              <div className="flex-shrink-0"></div>
-              <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
+              <div className="flex-shrink-0">
+                <FaChevronRight className="text-blue-500 mr-2 text-base text-2xl sm:text-lg relative top-1" />
 
-              <div className="ml-4">
-                <h3 className="text-2xl font-semibold text-white">
-                  Flexibilidad en la Alimentación{" "}
-                </h3>
-                <p className="text-gray-600 mt-2 text-white">
-                  Diseñaremos un plan que incluya solo comida que disfrutes y
-                  que ya tengas en casa, sin alimentos que te desagraden. Solo
-                  comerás alimentos que ya comes a diario, pero ajustare todo
-                  para conseguir los cambios deseados.{" "}
-                </p>
+                <div className="ml-4">
+                  <h3 className="text-2xl font-semibold text-white">
+                    Flexibilidad en la Alimentación{" "}
+                  </h3>
+                  <p className="text-gray-600 mt-2 text-white">
+                    Diseñaremos un plan que incluya solo comida que disfrutes y
+                    que ya tengas en casa, sin alimentos que te desagraden. Solo
+                    comerás alimentos que ya comes a diario, pero ajustare todo
+                    para conseguir los cambios deseados.{" "}
+                  </p>
+                </div>
               </div>
             </div>
 
             <div className="flex">
-              <div className="flex-shrink-0"></div>
-              <FaChevronRight className="text-blue-500 mr-2 text-base md:text-lg lg:text-xl relative top-1" />
-              <div className="ml-4">
-                <h3 className="text-2xl font-semibold text-white">
-                  Ahorro a Largo Plazo{" "}
-                </h3>
-                <p className="text-gray-600 mt-2 text-white">
-                  Invierte en tu conocimiento para reducir los costos
-                  recurrentes de las asesorías perpetuas.{" "}
-                </p>
+              <div className="flex-shrink-0">
+                <FaChevronRight className="text-blue-500 mr-2 text-base md:text-lg lg:text-xl relative top-1" />
+                <div className="ml-4">
+                  <h3 className="text-2xl font-semibold text-white">
+                    Ahorro a Largo Plazo{" "}
+                  </h3>
+                  <p className="text-gray-600 mt-2 text-white">
+                    Invierte en tu conocimiento para reducir los costos
+                    recurrentes de las asesorías perpetuas.{" "}
+                  </p>
+                </div>
               </div>
             </div>
 
