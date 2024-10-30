9df7ed2e03bce72f01f74a2da2efa016bcbecf58
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 01:30:57 -0300

    as

--- src/app/components/pro.tsx
+++ src/app/components/pro.tsx
@@ -4,7 +4,6 @@ const link = document.createElement("link");
 link.href =
   "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap";
 link.rel = "stylesheet";
-document.head.appendChild(link);
 
 function Features() {
   return (
