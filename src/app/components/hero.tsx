003ef733b485571cdd6d9adf8738c4a35fe6a853
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 02:59:28 -0300

    gaagdadgdag

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -11,7 +11,7 @@ function Hero() {
           </h1>
         </div>
 
-        <p className="text-x2 md:text-2xl lg:text-2xl mt-4 font-sans text-lg text-white">
+        <p className="text-1x1 md:text-1xl lg:text-2xl mt-4 font-sans text-lg text-white">
           &quot;Conmigo aprenderás a tomar el control de tu nutrición y
           entrenamiento para lograr resultados visibles y evitar errores que te
           hagan perder el tiempo. Te ayudaré a alcanzar tus objetivos y te
@@ -29,7 +29,7 @@ function Hero() {
             allowFullScreen
           ></iframe>
         </div>
-        <button className="pb-2 -translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
+        <button className="-translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-5 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
           <span className="text-lg">Reserva una llamada conmigo</span>
           <span className="text-sm mt-1 opacity-80">
             y empieza tu transformación
