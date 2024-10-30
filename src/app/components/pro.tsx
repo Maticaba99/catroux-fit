8a19b6acbdfbc650b6e50aa9c39d392a47b13722
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-10-30 05:25:38 -0300

    Update pro.tsx

--- src/app/components/pro.tsx
+++ src/app/components/pro.tsx
@@ -4,7 +4,7 @@ import "@fontsource/poppins";
 
 function Features() {
   return (
-    <div className="bg-gray-800 py-10">
+    <div className="bg-gray-800 py-14">
       <div className="max-w-7xl mx-auto px-4">
         <div style={{ fontFamily: "Poppins, sans-serif" }}>
           <h2 className="text-3xl md:text-4xl lg:text-5xl text-center text-white">
