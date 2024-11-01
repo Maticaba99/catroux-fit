04a79879e2ff95fca7811b70ec06fff99c0fd8c6
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-11-01 09:18:45 -0300

    Update hero.tsx

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -36,7 +36,7 @@ function Hero() {
           </div>
         </div>
 
-        <section className="py-5">
+        <section className="mt-32">
           <button className="-translate-y-2 mt-8 bg-blue-600 text-white font-semibold py-5 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
             <span className="text-lg">Reserva una llamada conmigo</span>
             <span className="text-sm mt-1 opacity-80">
