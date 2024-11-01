6ef11400206ae5c19b610a027882cd9b7f52a66b
thiagovc10 <thiagooocabalerooo@gmail.com>
2024-11-01 09:35:22 -0300

    Update hero.tsx

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className="py-10 bg-gray-800">
       <section className="flex flex-col items-center text-center space-y-4 min-w-64 mx-auto">
         <h1
-          className="font-bold text-center leading-tight text-3xl md:text-4xl lg:text-4xl  text-white m-4"
+          className="font-bold text-center leading-tight text-3xl md:text-4xl lg:text-4xl text-white m-4"
           style={{
             fontFamily: "Poppins, sans-serif",
             maxWidth: "1000px",
@@ -17,16 +17,16 @@ function Hero() {
         </h1>
 
         <p
-          className="text-1x1 md:text-1xl lg:text-1xl mt-4 font-sans text-lg text-white"
+          className="text-1x1 md:text-1xl lg:text-1xl mt-2 mb-6 font-sans text-lg text-white"
           style={{ fontFamily: "Poppins, sans-serif" }}
         >
           &quot;Aprende conmigo a tomar el control de tu nutrición y
           entrenamiento, obten resultados y sigue progresando sin depender de
           nadie&quot;.
         </p>
-        <div className="relative w-full max-w-full overflow-hidden">
-          <div className="flex justify-center  h-screen">
-            {" "}
+
+        <div className="relative w-full max-w-full overflow-hidden mt-4">
+          <div className="flex justify-center h-screen">
             <iframe
               className="max-w-6xl w-full h-3/3"
               src="https://player.vimeo.com/video/359553297"
@@ -37,7 +37,7 @@ function Hero() {
         </div>
 
         <section className="mt-32">
-          <button className="-translate-y-2  bg-blue-600 text-white font-semibold py-5 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
+          <button className="-translate-y-2 bg-blue-600 text-white font-semibold py-5 px-8 rounded-lg shadow-lg transform transition-all duration-300 hover:bg-blue-700 hover:scale-105 hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-blue-300 flex flex-col items-center">
             <span className="text-lg">Reserva una llamada conmigo</span>
             <span className="text-sm mt-1 opacity-80">
               y empieza tu transformación
