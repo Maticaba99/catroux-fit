301b124bb9a8fae36b5763d18cf98eb240578e91
Matias Caballero <maticaba99@hotmail.com>
2024-11-09 12:56:51 -0300

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="bg-[#171c2b] text-white px-4 sm:px-6 lg:px-8">
       <div className="flex justify-center items-center md:px-6 2xl:px-11 py-8">
         <img
-          src="/logo-clean-new.png"
+          src="/logo-clean.png"
           alt="logo"
           className="flex items-center w-36"
         />
